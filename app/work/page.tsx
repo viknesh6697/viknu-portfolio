badc65919525c1d909696272e3ce96cb028247ad
viknesh6697 <viknesh6697+github@gmail.com>
2026-03-24 22:08:03 +0530

    intial

--- app/work/page.tsx
+++
@@ -1,9 +0,0 @@
-import Projects from "@/components/sections/Projects";
-
-export default function WorkPage() {
-  return (
-    <section className="py-16">
-      <Projects />
-    </section>
-  );
-}
